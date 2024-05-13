f966a1abf38a03840299c4126c1080d15c40163b
Ko4Learner <teterev.vakuum@mail.ru>
2024-05-13 22:22:49 +0300

    13.05.2024 +simple navigation

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -3,8 +3,7 @@ package com.practicum.playlistmaker
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
