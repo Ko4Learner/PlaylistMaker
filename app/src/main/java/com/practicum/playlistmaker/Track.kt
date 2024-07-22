ba23a6f423d27cebc46fce898cde470bcdc68315
Ko4Learner <teterev.vakuum@mail.ru>
2024-07-22 22:53:16 +0300

    22.07.2024 Спринт № 13

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -8,7 +8,9 @@ data class Track(
     val artistName: String,
     @SerializedName("trackTimeMillis")
     val trackTime: Long,
-    val artworkUrl100: String
-) {
-
-}
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String,
+)
