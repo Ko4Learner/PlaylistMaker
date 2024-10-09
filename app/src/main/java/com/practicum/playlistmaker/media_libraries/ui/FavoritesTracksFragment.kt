b3c47c24df451219b49a8175dada722df3e58bef
Ko4Learner <teterev.vakuum@mail.ru>
2024-10-09 17:30:18 +0300

    Спринт № 18

---
+++ app/src/main/java/com/practicum/playlistmaker/media_libraries/ui/FavoritesTracksFragment.kt
@@ -0,0 +1,4 @@
+package com.practicum.playlistmaker.media_libraries.ui
+
+class FavoritesTracksFragment {
+}
--- app/src/main/java/com/practicum/playlistmaker/media_libraries/ui/MediaLibrariesActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/media_libraries/ui/MediaLibrariesActivity.kt
@@ -2,9 +2,7 @@ package com.practicum.playlistmaker.media_libraries.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityMediaLibrariesBinding
 
 class MediaLibrariesActivity : AppCompatActivity() {
@@ -12,10 +10,8 @@ class MediaLibrariesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMediaLibrariesBinding.inflate(LayoutInflater.from(this))
-
-        setContentView(R.layout.activity_media_libraries)
+        setContentView(binding.root)
 
     }
 }
